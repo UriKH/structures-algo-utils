a2be94519e41a28c9f868468c52bf074946f976c
UriKH <urikh@icloud.com>
2023-02-14 11:57:00 +0200

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,47 +1,15 @@
 #include <iostream>
+#include "stack.h"
+#include <string>
 
-template <typename T>
-class Node{
-private:
-	T val;
-	Node *next;
-
-public:
-	Node(T val, Node *node): val(val), next(node){}
-	
-	Node(T val){
-		this->val = val;
-		this->next = nullptr;
-	}
-
-	Node Node(Node other){
-		return Node(other.val, nullptr);
-	}
-
-	T get_val() const{
-		return val;
-	}
-
-	Node* get_next() const{
-		return next;
-	}
-
-	void set_next(Node *next){
-		this->next = next;
-	}
-};
-
-template <typename T>
-class Stack: public Node{
-private:
-	Node *head;
-
-private:
-	Stack(T val){
-		this->head = new Node(val, nullptr);
-	}
-
-	bool append(T val){
-		this->head->set_next(new Node(val, nullptr));
-	}
-};
+int main() {
+	Stack<std::string> s;
+	s.push("hi!");
+	std::cout << s.top() << std::endl;
+	std::cout << s.length() << std::endl;
+	std::cout << s.pop() << std::endl;
+	std::cout << s.length() << std::endl;
+	std::cout << s.pop() << std::endl;
+	std::cout << s.length() << std::endl;
+	return 0;
+}
